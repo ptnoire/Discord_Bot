9bdfb70f696f1a02d2adf0b58fba815fc7cc794b
ptnoire <ptnoire@gmail.com>
2023-03-28 14:03:10 -0400

    Discord can detect certain events, have an outline to how to use it

--- src/bot.js
+++ src/bot.js
@@ -1,21 +1,64 @@
-const Discord = require('discord.js');
+const {Client, Events, GatewayIntentBits, Partials } = require('discord.js');
 const { token } = require('../config.json');
-const client = new Discord.Client({
-    intents: [
-        Discord.IntentsBitField.Flags.GuildMessages,
-    ]
+
+const ints = { intents: [
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages, 
+    GatewayIntentBits.MessageContent, 
+    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.GuildMessageReactions,
+    ],
+    partials: [
+        Partials.Channel, 
+        Partials.Message, 
+        Partials.Reaction
+    ],
+}
+
+const client = new Client(ints);
+
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
 });
-const server = client.guilds.cache.find(guild => guild.name === 'Bot Test Server');
 
+client.on('messageCreate', (msg) => {
+    const messageData = {
+        channel: msg.channel.name,
+        member: msg.author.tag,
+        author: msg.member.nickname,
+        content: msg.content,
+        timestamp: new Date(msg.createdTimestamp),
+        id: msg.id,
+    }
+    console.log(messageData);
+})
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
-});
+client.on('messageDelete', (msg) => {
+    console.log(msg.id);
+    // Delete Message in DataBase
+})
 
-// client.on('message', msg => {
+client.on('guildScheduledEventCreate', (ev) => {
+    const eventData = {
+        name: ev.name,
+        description: ev.description,
+        location: ev.entityMetadata.location,
+        timeStart: new Date(ev.scheduledStartTimestamp),
+        timeEnd: new Date(ev.scheduledEndTimestamp),
+        id: ev.id,
+        eventCreatedAt: new Date(),
+    }
+    console.log(eventData);
+    // POST to database
+})
 
-// })
+client.on('guildScheduledEventDelete', (evC) => {
+    console.log(evC);
+    // Find evC.id -> Delete From DataBase
+})
 
-server.systemChannel.send('Test Message');
+// DEBUG LOG
+// client.on('debug', (info) => console.log(`Discord Bot Debug: ${info}`));
+client.on('error', (err) => console.error(`Discord Bot Error: ${err}`));
 
 client.login(token);
